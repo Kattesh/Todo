1a2c627bc64147766e22dc1bf63e51e370d959fc
Kate.sh <kate.sh@list.ru>
2022-06-12 15:59:22 -0500

    16.6.12

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,6 @@
 import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
 import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
-import {applyMiddleware, combineReducers, createStore} from 'redux'
+import {applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {appReducer} from './app-reducer'
 import {authReducer} from "../features/login/auth-reducer";
